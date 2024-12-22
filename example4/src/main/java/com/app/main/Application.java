60dd2101b1135d31c831e2a559b6dae8b929318e
Uday <budayteja009@gmail.com>
2024-12-22 15:04:46 +0530

    Primary Annotation

---
+++ example4/src/main/java/com/app/main/Application.java
@@ -0,0 +1,21 @@
+package com.app.main;
+
+import com.app.beans.Vehicle;
+import com.app.config.ProjectConfiguration;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
+
+        Vehicle vehicle = context.getBean(Vehicle.class);
+
+        System.out.println("Spring Context : " + vehicle.getName());
+
+
+    }
+}
