c343f76a51e777f84c09890d8b5ea57c3c895d78
Uday <budayteja009@gmail.com>
2025-01-02 23:18:27 +0530

    Hello World Web App using SpringBoot & Spring MVC

---
+++ example18/src/main/java/com/app/example18/Example18Application.java
@@ -0,0 +1,13 @@
+package com.app.example18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example18Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example18Application.class, args);
+    }
+
+}
